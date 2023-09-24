4ee4251c3d3950d828057541e17293d357328ca3
GoktugResmi <143345012+GoktugResmi@users.noreply.github.com>
2023-09-24 19:24:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = ""
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = "20284546"
+    API_HASH = "8d649e2ae1b14571156443721f41cd6a"
+    TOKEN = "6667777415:AAHBUdcgoSWTmDxXctgJ76yvSM2PdVTgqdo"  
+    START_PIC = "https://graph.org/file/03b3189033e01a856d699.jpg" 
+    CHAT = "-1001978170398"    
